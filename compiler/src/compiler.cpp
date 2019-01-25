138d5c6d9772975597e9861f5cc42d5254462371
okkindel <ebrithil97@gmail.com>
2019-01-26 00:03:15 +0100

    last touch

--- compiler/src/compiler.cpp
+++ compiler/src/compiler.cpp
@@ -285,7 +285,7 @@ void setRegister(string reg, long long int num) {
             insert("INC", reg);
         }
     // some registers are breaking
-    } else if (worth && (reg != "B") && (reg != "C") && (reg != "D")) {
+    } else if (worth && (reg != "A") && (reg != "B") && (reg != "C") && (reg != "D")) {
         if (reg != lowest_reg) {
             insert("COPY", reg, lowest_reg);
         }
